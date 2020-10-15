e02979051ed42be6893d5674732aff5ca6f95988
Grzegorz Jeremenko <grzesiula002@gmail.com>
2020-10-15 23:40:07 +0200

    Add routing

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import Login from './pages/Login.vue'
+import Home from './pages/Home.vue'
+
+const routes = [
+    { path: '/home', component: Home },
+    { path: '/', component: Login }
+];
+
+export default routes;
