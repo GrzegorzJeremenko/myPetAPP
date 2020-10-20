c215add0a04026800d5f7abd47a9b7dd4eb371e2
Grzegorz Jeremenko <grzesiula002@gmail.com>
2020-10-20 13:13:12 +0200

    Add animations to welcome page

--- src/routes.js
+++ src/routes.js
@@ -20,12 +20,12 @@ export const router = new Router({
 })
 
 router.beforeEach((to, from, next) => {
-    const publicPages = ['/login', '/register']
+    const publicPages = ['/login', '/register', '/welcome']
     const authRequired = !publicPages.includes(to.path)
     const loggedIn = localStorage.getItem('user')
 
     if (authRequired && !loggedIn) {
-        return next('/login')
+        return next('/welcome')
     }
 
     next()
