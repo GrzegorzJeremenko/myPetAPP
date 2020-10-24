2f15a0960c8271cb502201981ec87762a84ed9b6
Grzegorz Jeremenko <grzesiula002@gmail.com>
2020-10-24 12:45:45 +0200

    Fix notifications

--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -1,8 +1,8 @@
 <template>
-  <section id="notification">
+  <section class="notification">
         <i class="icon">&#xE808;</i>
         <h1>
-            <b>{{ title }}</b>{{ desc }}
+            <b>{{ element.title }}</b>{{ element.desc }}
         </h1>
         <i 
             class="icon"
@@ -17,31 +17,32 @@
 <script>
   export default {
     name: 'Notification',
-    props: ['title', 'desc'],
+    props: ['element'],
     methods: {
-        close() {
-            this.$el.parentNode.removeChild(this.$el)
-        }
+      close() {
+        this.$emit('close', this.element);
+      }
     }
   }
 </script>
 
 <style scoped>
-  section#notification {
-    width: 100%;
+  section.notification {
+    width: 90%;
     height: 40px;
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: space-between;
+    border-top: 1px solid #ddd;
   }
 
-  section#notification i {
+  section.notification i {
     font-size: 16px;
     margin: 0 10px 0 10px;
   }
 
-  section#notification b {
+  section.notification b {
     font-weight: 600;
   }
 </style>
--- src/components/NotificationBar.vue
+++ src/components/NotificationBar.vue
@@ -1,37 +1,58 @@
 <template>
   <section id="notificationBar">
-    <ol>
-        <li 
-            v-for="notification in notifications"
-            :key="notification.id"
-        >
-            <notification
-                v-bind:title="notification.title"
-                v-bind:desc="notification.desc"
-            />
-        </li>
-    </ol>
+    <notification
+      v-for="notification in notifications"
+      :key="notification.id"
+      v-bind:element="notification"
+      v-on:close="closeNotifi"
+    />
+    <h1 v-show="this.notifications.length == 0">Brak powiadomień</h1>
   </section>
 </template>
 
 <script>
-    import Notification from './Notification.vue';
+  import Notification from './Notification.vue';
 
   export default {
     name: 'NotificationBar',
     components: {
-        Notification
+      Notification
     },
     data() {
       return {
         notifications: [
-            {
-                id: 0,
-                title: 'Niska temeperatura - ',
-                desc: 'Pimpek',
-            }
-        ]
+          {
+            id: 0,
+            title: 'Niska temeperatura - ',
+            desc: 'Pimpek',
+          },
+          {
+            id: 1,
+            title: 'Niska temeperatura - ',
+            desc: 'Pimpek2',
+          },
+          {
+            id: 2,
+            title: 'Niska temeperatura - ',
+            desc: 'Pimpek3',
+          },
+          {
+            id: 3,
+            title: 'Niska temeperatura - ',
+            desc: 'Pimpek4',
+          }
+        ],
+        notifiCount: 0
       }
+    },
+    methods: {
+      closeNotifi(element) {
+        const notifiIndex = this.notifications.indexOf(element);
+        this.notifications.splice(notifiIndex, 1);
+      }
+    },
+    created: function () {
+      this.notifiCount = this.notifications.length;
     }
   }
 </script>
@@ -43,25 +64,9 @@
     width: 100%;
     color: #fff;
     background-color: rgb(85, 22, 118);
-    height: 5rem;
+    min-height: 2rem;
     display: flex;
     flex-direction: column;
-    align-items: center;
-  }
-
-  section#notificationBar ol {
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-
-  section#notificationBar ol li {
-    width: 90%;
-    height: 40px;
-    border-bottom: 1px solid #ddd;
-    display: flex;
-    flex-direction: row;
     align-items: center;
   }
 </style>
